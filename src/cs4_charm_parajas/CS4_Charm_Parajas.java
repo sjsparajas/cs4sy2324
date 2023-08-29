2591725fe48f41d64950ae52f69001f4fca3e745
CHARM <CHARM@172.22.16.77>
2023-08-29 16:14:09 +0800

    First Java Project

---
+++ src/cs4_charm_parajas/CS4_Charm_Parajas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4_charm_parajas;
+
+/**
+ *
+ * @author CHARM
+ */
+public class CS4_Charm_Parajas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        string
+        
+    }
+    
+}
